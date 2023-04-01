f200c05d55a4e0e2ce9e3b5e4465bc92aeead82c
AnkuRana <amitrana.com0007@gmail.com>
2023-04-01 12:20:19 +0530

    merge sort using c sharp

--- DataStructures/LinkedList/Program.cs
+++ DataStructures/LinkedList/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using CustomLinkedList;
+using LinkedList;
+
 namespace Datastructure
 {
     class Program { 
@@ -12,37 +14,43 @@ namespace Datastructure
             //Console.WriteLine(number);
 
             // Create list
-            linked l1 = new linked();
-            l1.add_node(1);
-            l1.add_node(2);
-            l1.add_node(3);
-            l1.add_node(4);
-            l1.add_node(5);
-            Console.WriteLine("Linked List:");
-            l1.print_list();
+            //linked l1 = new linked();
+            //l1.add_node(1);
+            //l1.add_node(2);
+            //l1.add_node(3);
+            //l1.add_node(4);
+            //l1.add_node(5);
+            //Console.WriteLine("Linked List:");
+            //l1.print_list();
 
-            //using method 1
-            Console.WriteLine("Reverse linked list");
-            l1.reverse();
-            l1.print_list();
+            ////using method 1
+            //Console.WriteLine("Reverse linked list");
+            //l1.reverse();
+            //l1.print_list();
             
-            //using method 2
-            Console.WriteLine("Reverse linked list using recurssion method 2");
-            if (l1.head == null)
-            {
-                Console.WriteLine("Linked list is emnpty");
-            }
-            else
-            {
-                l1.head = l1.reverse_rec(l1.head);
-            }
-            l1.print_list();
+            ////using method 2
+            //Console.WriteLine("Reverse linked list using recurssion method 2");
+            //if (l1.head == null)
+            //{
+            //    Console.WriteLine("Linked list is emnpty");
+            //}
+            //else
+            //{
+            //    l1.head = l1.reverse_rec(l1.head);
+            //}
+            //l1.print_list();
 
-            //using method 3
-            Console.WriteLine("Reverse linked list uisng method 3");
-            l1.rev_util();
-            l1.print_list();
-
+            ////using method 3
+            //Console.WriteLine("Reverse linked list uisng method 3");
+            //l1.rev_util();
+            //l1.print_list();
+            mergeSort obj = new mergeSort();
+            int[] arr = new int[] { 1, 4, 5, 2, 7, 3, 6, 9, 8,4, 32 ,18, 12, 21,19 ,87,65,100, 99 };
+            Console.WriteLine("Before sorting:");
+            obj.printArray(arr);
+            obj.msort(arr, 0, arr.Length-1);
+            Console.WriteLine("After Sorting:");
+            obj.printArray(arr);
         }
 
     }
---
+++ DataStructures/LinkedList/mergeSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class mergeSort
+    {
+        public void msort(int[] arr, int l , int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                msort(arr, l, m);
+                msort(arr, m + 1, r);
+                merge(arr, l, r, m);
+            }
+        }
+
+        void merge(int[] arr, int l, int r, int m)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] larr = new int[n1];
+            int[] rarr = new int[n2];
+
+            for (int i = 0; i < n1; ++i)
+            {
+                larr[i] = arr[l + i];
+            }
+            for (int i = 0; i < n2; ++i)
+            {
+                rarr[i] = arr[m + 1 + i];
+            }
+            int p = 0, q = 0, k = l;
+            while (p < n1 && q < n2)
+            {
+                if (larr[p] >= rarr[q])
+                {
+                    arr[k] = rarr[q];
+                    q++;
+                }
+                else
+                {
+                    arr[k] = larr[p];
+                    p++;
+                }
+                k++;
+            }
+
+            while (p < n1)
+            {
+                arr[k] = larr[p];
+                p++;
+                k++;
+            }
+            while (q < n2)
+            {
+                arr[k] = rarr[q];
+                q++;
+                k++;
+            }
+
+
+        }
+        public void printArray(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
