f6c2e3142edc3feeedf00969e152bf3d8a41a2e3
AnkuRana <amitrana.com0007@gmail.com>
2023-04-02 11:38:07 +0530

    change to use swap function from helper class

---
+++ DataStructures/LinkedList/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class BubbleSort
+    {
+        static int count=0;
+        public void bubblesort(int[] arrt)
+        {
+            bool flag = false;
+            for (int i = 0; i < arrt.Length; i++)
+            {
+                for (int j = 0; j < arrt.Length - 1; j++)
+                {
+                    if (arrt[j] > arrt[j + 1])
+                    {
+                        flag = true;
+                        // swap numbers in  array at following indicies
+                        new HelperClass().swap(arrt, j, j + 1);
+                    }
+                    if (!flag)
+                    {
+                        Console.WriteLine($"There is no more comparison left. Count: {count}");
+                        return;
+                    }
+
+                }
+                count++;
+            }
+        }
+    }
+}
