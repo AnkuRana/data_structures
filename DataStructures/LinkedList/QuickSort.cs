e088d5399c0f099412f93f93b5a1ff49a71c772e
AnkuRana <amitrana.com0007@gmail.com>
2023-04-02 11:36:04 +0530

    quick sort using csharp

---
+++ DataStructures/LinkedList/QuickSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class QuickSort
+    {
+        public void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = partition(arr, low, high);
+                quickSort(arr, low, pi-1);
+                quickSort(arr, pi+1, high);
+
+            }
+            
+        }
+
+        int partition(int[] arr, int low, int high)
+        {
+            int i = low - 1;
+            int pivot = arr[high];
+            for (int j = low; j < high; j++)
+            {
+                if (pivot >= arr[j])
+                {
+                    i++;
+                    new HelperClass().swap(arr, i, j);
+                }
+            }
+            new HelperClass().swap(arr, i + 1, high);
+
+            return i+1;
+        }
+        
+        
+    }
+}
