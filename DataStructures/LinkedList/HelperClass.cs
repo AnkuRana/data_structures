f617cabf8adc41f9d8d34bcb02ff61f92e0afc52
AnkuRana <amitrana.com0007@gmail.com>
2023-04-02 11:37:13 +0530

    Added helper class
    two methods , one printing array and other swaping number in array the given indices.

---
+++ DataStructures/LinkedList/HelperClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class HelperClass
+    {
+        public void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+
+        }
+        public void swap(int[] array, int first, int second)
+        {
+            int temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
