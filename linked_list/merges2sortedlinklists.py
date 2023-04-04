3c705003a2e5e7f779c553a43e80d670d7fd6b23
AnkuRana <amitrana.com0007@gmail.com>
2023-04-04 20:47:59 +0530

    Merge two sorted linked list again in sorted manner using recursion

---
+++ linked_list/merges2sortedlinklists.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+
+class linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def insert_node(self, curr_node, value):
+        if self.head is None:
+            self.head = Node(value)   
+        elif curr_node.next is None:
+            curr_node.next = Node(value)
+            return
+        else:
+            self.insert_node(curr_node.next, value)
+            
+def print_ll(head):
+        if head is None:
+            print("Empty linked list.")
+        else:
+            temp = head
+            while temp is not None:
+                print(f"{temp.value}",end="->")
+                temp = temp.next
+
+def mergeSortedlinkedlists(head_1, head_2):
+    if head_1 is None:
+        return head_2
+    if head_2 is None: 
+        return head_1
+    
+    if head_1.value > head_2.value:
+        head_2.next = mergeSortedlinkedlists(head_1, head_2.next)
+        return head_2
+    else:
+        head_1.next = mergeSortedlinkedlists(head_1.next, head_2)
+        return head_1
+            
+l1 = linkedlist()
+l2 = linkedlist()
+first = [1, 3, 7, 8, 9]
+second = [2, 4, 5, 6, 10]
+for i in range(len(first)):
+    l1.insert_node(l1.head, first[i])
+for i in range(len(second)):
+    l2.insert_node(l2.head, second[i])
+print("first linked list: ", end="")
+print_ll(l1.head)
+print()
+print("second linked list: ", end="")
+print_ll(l2.head)
+print()
+merge_head = mergeSortedlinkedlists(l1.head, l2.head)
+print("Merged linked list: ", end="")
+print_ll(merge_head)
+
+       
+            
+        
